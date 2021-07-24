bbe8fc3e34a2b56e2912be10b2a4f675205d8580
Elliot <48284356+190101101@users.noreply.github.com>
2021-07-24 22:04:16 +0400

    Add files via upload

---
+++ libs/files/js/main.js
@@ -0,0 +1,32 @@
+$(function(){
+	 var forEach=function(t,o,r){if("[object Object]"===Object.prototype.toString.call(t))for(var c in t)Object.prototype.hasOwnProperty.call(t,c)&&o.call(r,t[c],c,t);else for(var e=0,l=t.length;l>e;e++)o.call(r,t[e],e,t)};
+    var hamburgers = document.querySelectorAll(".hamburger");
+    if (hamburgers.length > 0) {
+      forEach(hamburgers, function(hamburger) {
+        hamburger.addEventListener("click", function() {
+          this.classList.toggle("is-active");
+        }, false);
+      });
+    }
+ });
+
+/*audio*/
+$(function(){
+  $('.play').on('click', function(){
+    $('.play').toggleClass('audioactive');
+  });
+});
+
+var myAudio = document.getElementById("rhapsody");
+var isPlaying = false;
+
+function togglePlay() {
+  isPlaying ? myAudio.pause() : myAudio.play();
+};
+
+myAudio.onplaying = function() {
+  isPlaying = true;
+};
+myAudio.onpause = function() {
+  isPlaying = false;
+};
